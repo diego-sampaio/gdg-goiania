27fe3a07a65cf011652b7aecb691b468e2a74699
Diego Sampaio <diegosampaio18@gmail.com>
2024-11-27 18:11:43 -0300

    fix: next configs

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@
 const nextConfig = {
   output: 'export', // generate static files for GitHub Pages
   basePath: '/gdg-goiania',
+  assetPrefix: '/gdg-goiania/',
   images: {
     unoptimized: true, // avoid image optimization
   },
